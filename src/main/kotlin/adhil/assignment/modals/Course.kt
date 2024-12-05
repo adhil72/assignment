84143625191ec79ecd02a734b4a9148899effcc9
Muhammed adhil <126852465+adhil72@users.noreply.github.com>
2024-12-05 16:08:40 +0530

    Update Course.kt

--- src/main/kotlin/adhil/assignment/modals/Course.kt
+++ src/main/kotlin/adhil/assignment/modals/Course.kt
@@ -11,7 +11,5 @@ data class Course(
     val createdAt: String,
     val createdBy: String?
 ) {
-    constructor(title: String, description: String, price: Double, createdBy: String) : this(UUID.randomUUID().toString(), title, description, price,0, "",createdBy){
-
-    }
+    constructor(title: String, description: String, price: Double, createdBy: String) : this(UUID.randomUUID().toString(), title, description, price,0, "",createdBy)
 }
